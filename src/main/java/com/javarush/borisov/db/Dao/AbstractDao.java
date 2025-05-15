8a78ccd7d8b4f44390aa2cfaab1ef9bd67d696cf
Sergey Borisov <sergant@nk.ru>
2025-05-15 23:07:01 +0700

    Сделан AbstractDao
    TODO
    !ДАО
    добавить временные таблицы для оборудования организовать маппинг
    в дао не забыть удалять запись из временной таблицы и добавлять в реальную
    
    Вынести константы
    По умолчанию отображение заявок "Назначено";
    
    Добавление контрагентов с кнопки + возможность добавления своих колонок (не приоритетно);
    Добавить оборудование;
    Добавить склад;
    
    Каждой заявке добавить возможность редактирования в таблице отображения;
    Добавить списание / поступление оборудования на основе заявки;
    Кнопка ручного добавления оборудования на склад;
    Кнопка ручного добавления заявки + сериализация добавленной заявки;
    Переписать БД чтобы не читать с диска где что лежит, а при изменении заявки добавлять инфо в поля БД(возможно оставить методы для синхронизации с диском );
    
    Добавить Акты(обдумать)
    В отображении заявок добавить Печать актов
    Вывод окна ошибки (не приоритетно)
    Логи (скорее всего во время бетта теста для большего понимания что нужно)
    Нормально отловить ошибки(скорее всего на бетте)
    Подумать по фронту (см https://htmx.org/docs/)
    Паттерн build для заявок (подумать надо ли, не приоритетно)
    
    Заменить BD на нормальную (после 4 модуля)
    Генерация ШК на основе номера заявки;
    Прикрутить бот;
    Прикрутить почтовый сервер;
    Прикрутить АПИ jira;
    Тесты (не приоритетно / опционально)

---
+++ src/main/java/com/javarush/borisov/db/Dao/AbstractDao.java
@@ -0,0 +1,73 @@
+package com.javarush.borisov.db.Dao;
+
+import com.javarush.borisov.config.MySessionCreator;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import jakarta.transaction.Transactional;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
+@Transactional
+public abstract class AbstractDao {
+
+
+    public <T> T getById(Class<T> clazz, Long id){
+        try (Session session = MySessionCreator.getSessionCreator().openSession()) {
+            return  session.get(clazz,id);
+
+        }
+    }
+
+    public <T> List<T> getAll(Class<T> clazz){
+        try (Session session = MySessionCreator.getSessionCreator().openSession()) {
+            return session.createQuery("from clazz.getSimpleName()", clazz).list();
+        }
+    }
+    public <T> T getSingleByField(Class<T> clazz, String fieldName, String value){
+        try (Session session = MySessionCreator.getSessionCreator().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<T> cq = cb.createQuery(clazz);
+            Root<T> root = cq.from(clazz);
+            cq.select(root).where(cb.like(root.get(fieldName).as(String.class),"%" + value + "%"));
+            return session.createQuery(cq).getSingleResult();
+       }
+    }
+    public <T> List<T> getListByField(Class<T> clazz, String fieldName, String value){
+        try (Session session = MySessionCreator.getSessionCreator().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<T> cq = cb.createQuery(clazz);
+            Root<T> root = cq.from(clazz);
+            cq.select(root).where(cb.equal(root.get(fieldName).as(String.class),value));
+            return session.createQuery(cq).getResultList();
+        }
+    }
+
+    public <T> void save(T entity){
+        try (Session session = MySessionCreator.getSessionCreator().openSession()) {
+                    session.save(entity);
+        }
+    }
+    public <T> void update(T entity){
+        try (Session session = MySessionCreator.getSessionCreator().openSession()) {
+                    session.update(entity);
+        }
+    }
+    public <T> void delete(T entity){
+        try (Session session = MySessionCreator.getSessionCreator().openSession()) {
+                    session.delete(entity);
+
+        }
+    }
+    public <T> void deleteById(Class<T> clazz, Long id){
+        try (Session session = MySessionCreator.getSessionCreator().openSession()) {
+                    session.delete(getById(clazz,id));
+        }
+    }
+    public <T> void deleteAll(Class<T> clazz){
+        try (Session session = MySessionCreator.getSessionCreator().openSession()) {
+                    session.delete(getAll(clazz));
+        }
+    }
+}
